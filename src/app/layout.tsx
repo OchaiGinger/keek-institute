dd9fb67c8b1ba14a0bc90272e20566a1dc377894
GingerOchai <samuelgingermichaelochai@gmail.com>
2026-04-09 14:17:26 +0100

    corrected globalscss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./";
+import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Providers } from "../components/provider";
 
