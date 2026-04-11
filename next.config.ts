77490f7e4cbf486e9cfe66f107b83c699616711c
GingerOchai <samuelgingermichaelochai@gmail.com>
2026-04-11 19:02:33 +0100

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "keekinstitute.t3.storage.dev",
+        hostname: "keek-institute.t3.storage.dev",
         port: "",
         protocol: "https",
       },
